feeb1ad3683d7f7fc1a975c6602920e0d6315d3d
lemonade0123 <gustmd6895@hanmail.net>
2025-04-23 14:18:51 +0900

    insufficient

---
+++ src/workshop/account/entity/Account.java
@@ -0,0 +1,63 @@
+package workshop.account.entity;
+
+public class Account extends Object {
+	
+	private String custId;
+	private String acctId;
+	private int balance;
+	
+	//(생성자)
+	public Account() {
+		System.out.println("Default Constructor Called..");
+	}
+	
+	//Constructor Overloading(생성자 중복)
+	public Account(String custId, String acctId, int balance) {
+		super();
+		this.custId = custId;
+		this.acctId = acctId;
+		this.balance = balance;
+	}
+	
+	//setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
+	
+	//getter
+	public int getBalance() {
+		return balance;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	//입금
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+	//출금
+	public void withdraw(int amount) {
+		if(amount > balance) {
+			System.out.println("잔액부족");
+		}
+		this.balance -= amount;
+	}
+	
+	//부모 클래스가 가진 Object의 toString() 메서드를 재정의(Overriding)
+	//Method Signature public String toString()
+	@Override
+	public String toString() {
+		return "Account [custId=" + custId + ", acctId=" + acctId + ", balance=" + balance + "]";
+	}
+
+}
